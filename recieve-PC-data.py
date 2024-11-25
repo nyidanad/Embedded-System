52ba5f78d96e009cb58641aa429dfc41f230f5a5
Nyíri Dániel <91250016+nyidanad@users.noreply.github.com>
2024-11-25 18:34:57 +0100

    #5 feat: recieve-PC-data.py v2.0 Now all data will be in a sigle string, and all infos have an unique signature to be able to split them up inside main.c

--- recieve-PC-data.py
+++ recieve-PC-data.py
@@ -1,36 +1,46 @@
-import time
-import serial
-import psutil
+import time, serial, psutil, platform
 from cpuinfo import get_cpu_info
-import platform
 
-# serial port and baud rate
-ser = serial.Serial('COM10', 115200)
+# Serial port and baud rate
+ser = serial.Serial('COM12', 115200)
 mem = psutil.virtual_memory()
+print('Receiving data from PC...')
 
-print("--- SYSTEM ---")
-print(f"{platform.system()} {platform.architecture()[0]}")
-print("\n--- CPU ---")
-print(f"brand:\t {get_cpu_info()['brand_raw']} {get_cpu_info()['hz_actual_friendly']}")
-print(f"vendor:\t {get_cpu_info()['vendor_id_raw']}")
-print(f"arch:\t {get_cpu_info()['arch']}")
-print(f"cores:\t {get_cpu_info()['family']}")
-print(f"threads: {get_cpu_info()['count']}")
-print("\nCPU Usage/Core:")
-for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1), start=1):
-    print(f"Core {i}: {percentage}%")
-print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+while(1):
+  try:
+    rx_data = ""
 
-print("\n--- VRAM ---")
-print(mem)
+    # System info - '0001'
+    rx_data += "0001\n"
+    rx_data += f"System: {platform.system()} {platform.architecture()[0]}\n"
+    rx_data += f"{str(psutil.users())}\n"
 
-print("\n--- PARTICIONS ---")
-print(psutil.disk_partitions())
+    # CPU info - '0002'
+    rx_data += "0002\n"
+    cpu_info = get_cpu_info()
+    rx_data += f"CPU: {cpu_info['brand_raw']} {cpu_info['hz_actual_friendly']}\n"
+    rx_data += f"Vendor: {cpu_info['vendor_id_raw']}\n"
+    rx_data += f"Architecture: {cpu_info['arch']}\n"
+    rx_data += f"Cores: {cpu_info['family']}\n"
+    rx_data += f"Threads: {cpu_info['count']}\n"
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1), start=1):
+      rx_data += f"Core {i}: {percentage}%\n"
+    rx_data += f"Total CPU Usage: {psutil.cpu_percent()}%\n"
 
-print("\n--- DISK USAGE ---")
-print(psutil.disk_usage('/'))
+    # Partitions info - '0003'
+    rx_data += "0003\n"
+    rx_data += f"{str(mem)}\n"
+    rx_data += f"{str(psutil.disk_partitions())}\n"
+    rx_data += f"{str(psutil.disk_usage('/'))}\n"
 
-print("\n--- USER(S) ---")
-print(psutil.users())
+    # Network info - '0004'
+    rx_data += "0004\n"
+    rx_data += f"Network Interfaces:\n{psutil.net_if_addrs()}\n\r"
 
-print("\n--- NET ----")
+    # Send the data over UART
+    ser.write(rx_data.encode('utf-8'))
+    print('Data sent to UART')
+    time.sleep(5)
+
+  except Exception as e:
+    print(str(e))
